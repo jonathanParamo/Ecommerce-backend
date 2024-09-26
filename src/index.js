3fe3c4c7c66dfbec17c52fc5202eba8afc78e449
jonathanParamo <jhonathan-and@outlook.com>
2024-09-26 17:43:22 -0500

    Update CORS configuration to allow specific origins

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ const corsOptions = {
     }
   },
   methods: "GET,POST,PATCH,DELETE",
-  allowedHeaders: 'Content-Type',
+  allowedHeaders: 'Content-Type, Authorization',
   credentials: true
 };
 
